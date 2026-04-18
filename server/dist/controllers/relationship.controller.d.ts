c15cd3433908949e9c0e718d27fecb3a679c5001
Eagleeliz <lizwanjiku573@gmail.com>
2026-04-18 07:30:16 +0300

    remove node_modules, dist and .env from tracking

--- server/dist/controllers/relationship.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare class RelationshipController {
-    create(req: Request, res: Response, next: NextFunction): Promise<void>;
-    delete(req: Request, res: Response, next: NextFunction): Promise<void>;
-    getRelatives(req: Request, res: Response, next: NextFunction): Promise<void>;
-    getCousins(req: Request, res: Response, next: NextFunction): Promise<void>;
-}
-export declare const relationshipController: RelationshipController;
-//# sourceMappingURL=relationship.controller.d.ts.map
